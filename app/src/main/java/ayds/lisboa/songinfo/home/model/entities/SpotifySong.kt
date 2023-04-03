c08ad3d33b12d6449e93789dcec1bf7bb935fb61
PalomaHernandez <palohernandez01@gmail.com>
2023-04-03 17:40:24 -0300

    Update SpotifySong.kt
    fun getDate()

--- app/src/main/java/ayds/lisboa/songinfo/home/model/entities/SpotifySong.kt
+++ app/src/main/java/ayds/lisboa/songinfo/home/model/entities/SpotifySong.kt
@@ -1,4 +1,5 @@
 package ayds.lisboa.songinfo.home.model.entities
+import java.text.SimpleDateFormat
 
 sealed class Song {
     data class SpotifySong(
@@ -11,7 +12,35 @@ sealed class Song {
         val imageUrl: String,
         var isLocallyStored: Boolean = false,
         var releaseDatePrecision: String
-    ) : Song()
+    ) : Song(){
+        fun getDate(): String =
+            when(releaseDatePrecision){
+                "day" -> releaseDate
+                "month" -> { val outFormat = SimpleDateFormat("MMMM, yyyy", Locale.US)
+                    outFormat.format(releaseDate)
+                }
+                "year" -> { val year = releaseDate.split("-").first()
+                    year + " " + calculadoraBiciesto.esBiciesto(year.parseInt())
+                }
+            }
+    }
+
+    object calculadoraBiciesto {
+
+        fun esBiciesto(year: Int): String {
+            if (year % 4 == 0) {
+                if (year % 100 == 0) {
+                    year % 400 == 0
+                } else {
+                    return "(leap year)"
+                }
+            } else {
+                return "(not a leap year)"
+            }
+        }
+    }
+
     object EmptySong : Song()
+
 }
 
--- app/src/main/java/ayds/lisboa/songinfo/home/view/SongDescriptionHelperImpl.kt
+++ app/src/main/java/ayds/lisboa/songinfo/home/view/SongDescriptionHelperImpl.kt
@@ -18,7 +18,7 @@ internal class SongDescriptionHelperImpl : SongDescriptionHelper {
                 }\n" +
                         "Artist: ${song.artistName}\n" +
                         "Album: ${song.albumName}\n" +
-                        "Year: ${song.year}"
+                        "Date: ${song.getDate()}"
             else -> "Song not found"
         }
     }
