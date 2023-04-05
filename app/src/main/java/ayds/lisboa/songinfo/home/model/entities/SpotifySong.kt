f4ea9b12a04fb8a6f8c7ee512bdd980528b658ed
Facundo Diz <facundodiz87@gmail.com>
2023-04-05 11:00:34 -0300

    Se elimino el formateo del dateRelease en el modelo

--- app/src/main/java/ayds/lisboa/songinfo/home/model/entities/SpotifySong.kt
+++ app/src/main/java/ayds/lisboa/songinfo/home/model/entities/SpotifySong.kt
@@ -14,40 +14,8 @@ sealed class Song {
         val spotifyUrl: String,
         val imageUrl: String,
         var isLocallyStored: Boolean = false
-    ) : Song(){
-        @RequiresApi(Build.VERSION_CODES.O)
-        fun getDate(): String =
-            when(releaseDatePrecision){
-                "day" -> releaseDate
-                "month" -> CalculatorYearMonth.converter(releaseDate)
-                "year" -> { val year = releaseDate.split("-").first()
-                    year + " " + CalculatorLeap.itsLeap(year.toInt())
-                }
-                else -> "Date not found"
-            }
-    }
+    ) : Song()
 
     object EmptySong : Song()
-
-    object CalculatorLeap {
-        fun itsLeap(year: Int): String =
-            if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
-                "(leap year)"
-            else
-                "(not a leap year)"
-
-    }
-
-    object CalculatorYearMonth {
-        @RequiresApi(Build.VERSION_CODES.O)
-        fun converter(date: String): String {
-                val month = date.split("-").get(1)
-                val monthName = Month.of(month.toInt()).toString()
-                val year = date.split("-").first()
-            return "$monthName, $year"
-        }
-
-    }
-
 }
 
