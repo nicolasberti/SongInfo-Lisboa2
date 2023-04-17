b6cd115fd41d5de1ca9682022d4c11f0985265bf
Facundo Diz <facundodiz87@gmail.com>
2023-04-17 16:49:36 -0300

    Update HomeViewInjector.kt

--- app/src/main/java/ayds/lisboa/songinfo/home/view/HomeViewInjector.kt
+++ app/src/main/java/ayds/lisboa/songinfo/home/view/HomeViewInjector.kt
@@ -1,12 +1,12 @@
 package ayds.lisboa.songinfo.home.view
 
-import CalculatorFactoryImpl
+import DateConverterFactoryImpl
 import ayds.lisboa.songinfo.home.controller.HomeControllerInjector
 import ayds.lisboa.songinfo.home.model.HomeModelInjector
 
 object HomeViewInjector {
 
-    val songDescriptionHelper: SongDescriptionHelper = SongDescriptionHelperImpl(CalculatorFactoryImpl())
+    val songDescriptionHelper: SongDescriptionHelper = SongDescriptionHelperImpl(DateConverterFactoryImpl())
 
     fun init(homeView: HomeView) {
         HomeModelInjector.initHomeModel(homeView)
