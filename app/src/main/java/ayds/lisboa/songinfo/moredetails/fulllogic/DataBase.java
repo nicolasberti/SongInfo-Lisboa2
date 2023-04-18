548ad679a7ce0b508bd95010e3396f4cd01cf3ac
PalomaHernandez <palohernandez01@gmail.com>
2023-04-18 15:17:23 -0300

    Refactor en Database.java

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/DataBase.java
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/DataBase.java
@@ -11,53 +11,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DataBase extends SQLiteOpenHelper {
-
-  public static void testDB()
-  {
-
-    Connection connection = null;
-    try
-    {
-      // create a database connection
-      connection = DriverManager.getConnection("jdbc:sqlite:./dictionary.db");
-      Statement statement = connection.createStatement();
-      statement.setQueryTimeout(30);  // set timeout to 30 sec.
-
-      //statement.executeUpdate("drop table if exists person");
-      //statement.executeUpdate("create table person (id integer, name string)");
-      //statement.executeUpdate("insert into person values(1, 'leo')");
-      //statement.executeUpdate("insert into person values(2, 'yui')");
-      ResultSet rs = statement.executeQuery("select * from artists");
-      while(rs.next())
-      {
-        // read the result set
-        System.out.println("id = " + rs.getInt("id"));
-        System.out.println("artist = " + rs.getString("artist"));
-        System.out.println("info = " + rs.getString("info"));
-        System.out.println("source = " + rs.getString("source"));
-
-      }
+    static final String URLConnection = "jdbc:sqlite:./dictionary.db";
+    private static Connection createConnection() throws SQLException {
+        return DriverManager.getConnection(URLConnection);
     }
-    catch(SQLException e)
-    {
-      // if the error message is "out of memory",
-      // it probably means no database file is found
-      System.err.println(e.getMessage());
+
+    private static Statement createStatement(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        statement.setQueryTimeout(30);
+        return statement;
     }
-    finally
-    {
-      try
-      {
-        if(connection != null)
-          connection.close();
-      }
-      catch(SQLException e)
-      {
-        // connection close failed.
-        System.err.println(e);
-      }
+
+    private static void showArtists(Statement statement) throws SQLException {
+        ResultSet rs = statement.executeQuery("select * from artists");
+        while(rs.next())
+        {
+            System.out.println("id = " + rs.getInt("id"));
+            System.out.println("artist = " + rs.getString("artist"));
+            System.out.println("info = " + rs.getString("info"));
+            System.out.println("source = " + rs.getString("source"));
+        }
     }
-  }
+
+    public static void testDB()
+    {
+        Connection connection = null;
+        try
+        {
+            connection = createConnection();
+            Statement statement = createStatement(connection);
+            showArtists(statement);
+        }
+        catch(SQLException e)
+        {
+            System.err.println(e.getMessage());
+        }
+        finally
+        {
+            try
+            {
+                if(connection != null)
+                    connection.close();
+            }
+            catch(SQLException e)
+            {
+                System.err.println(e);
+            }
+        }
+    }
 
   public static void saveArtist(DataBase dbHelper, String artist, String info)
   {
