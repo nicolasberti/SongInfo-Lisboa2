e115a18dab0a778dc72ef5e576e95df219d0c263
Facundo Diz <facundodiz87@gmail.com>
2023-05-08 21:43:29 -0300

    constantes en la clase FormatterInfo

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/presentation/FormatterInfo.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/presentation/FormatterInfo.kt
@@ -4,6 +4,14 @@ import ayds.lisboa.songinfo.moredetails.fulllogic.domain.entities.Artist
 
 class FormatterInfo {
 
+    companion object {
+        const val HTML_WIDTH = "<html><div width=400>"
+        const val HTML_FONT = "<font face=\"arial\">"
+        const val HTML_END = "</font></div></html>"
+        const val NO_RESULTS = "No results"
+        const val PREFIX_LOCALLY_STORED = "[*]"
+    }
+
     fun getInfoFromArtistInfo(artistInfo: Artist.ArtistImpl?): String{
         var info = artistInfo?.info
         if (artistInfo?.isLocallyStored == true)
--- app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/presentation/OtherInfoConstants.kt
+++
@@ -1,7 +0,0 @@
-package ayds.lisboa.songinfo.moredetails.fulllogic.presentation
-
-const val HTML_WIDTH = "<html><div width=400>"
-const val HTML_FONT = "<font face=\"arial\">"
-const val HTML_END = "</font></div></html>"
-const val NO_RESULTS = "No results"
-const val PREFIX_LOCALLY_STORED = "[*]"
