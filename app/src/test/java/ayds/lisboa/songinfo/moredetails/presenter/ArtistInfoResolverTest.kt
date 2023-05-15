d049f3db87f7a19b2d531fa4a4fdbbcbac1e7745
Facundo Diz <facundodiz87@gmail.com>
2023-05-15 19:19:10 -0300

    Update ArtistInfoResolverTest.kt
    Co-Authored-By: ezearce <89986409+ezearce@users.noreply.github.com>

--- app/src/test/java/ayds/lisboa/songinfo/moredetails/presenter/ArtistInfoResolverTest.kt
+++ app/src/test/java/ayds/lisboa/songinfo/moredetails/presenter/ArtistInfoResolverTest.kt
@@ -1,4 +1,52 @@
 package ayds.lisboa.songinfo.moredetails.presenter
 
+import ayds.lisboa.songinfo.moredetails.domain.entities.Artist
+import ayds.lisboa.songinfo.moredetails.domain.entities.Artist.LastFMArtist
+import ayds.lisboa.songinfo.moredetails.presentation.ArtistInfoResolver
+import ayds.lisboa.songinfo.moredetails.presentation.ArtistInfoResolverImpl
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+
 class ArtistInfoResolverTest {
+
+    private lateinit var artistInfoResolver: ArtistInfoResolver
+
+    @Before
+    fun setUp() {
+        artistInfoResolver = ArtistInfoResolverImpl()
+    }
+    @Test
+    fun `given artist info and name and isLocallyStored, when getFormattedInfo is called, then returns formatted info in html`() {
+        // Arrange
+        val artistInfo = LastFMArtist("Test artist", "Test bio","Test url",1,true)
+        val artistName = "Test artist"
+
+        // Act
+        val result = artistInfoResolver.getFormattedInfo(artistInfo, artistName)
+
+        // Assert
+        val expected =  "<html><div width=400><font face=\"arial\">[*]Test bio</font></div></html>"
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `given artist info and name and not isLocallyStored, when getFormattedInfo is called, then returns formatted info in html`() {
+        // Arrange
+        val artistInfo = LastFMArtist("Test artist", "Test bio","Test url",1,false)
+        val artistName = "Test artist"
+
+        // Act
+        val result = artistInfoResolver.getFormattedInfo(artistInfo, artistName)
+
+        // Assert
+        val expected =  "<html><div width=400><font face=\"arial\">Test bio</font></div></html>"
+        assertEquals(expected, result)
+    }
+
 }
