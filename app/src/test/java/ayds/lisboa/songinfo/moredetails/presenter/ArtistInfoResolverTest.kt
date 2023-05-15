dbc3c71b9944d1d6d6273df14afe386369e02484
Facundo Diz <facundodiz87@gmail.com>
2023-05-15 19:51:55 -0300

    Update ArtistInfoResolverTest.kt
    Co-Authored-By: ezearce <89986409+ezearce@users.noreply.github.com>

--- app/src/test/java/ayds/lisboa/songinfo/moredetails/presenter/ArtistInfoResolverTest.kt
+++ app/src/test/java/ayds/lisboa/songinfo/moredetails/presenter/ArtistInfoResolverTest.kt
@@ -2,6 +2,7 @@ package ayds.lisboa.songinfo.moredetails.presenter
 
 import ayds.lisboa.songinfo.moredetails.domain.entities.Artist
 import ayds.lisboa.songinfo.moredetails.domain.entities.Artist.LastFMArtist
+import ayds.lisboa.songinfo.moredetails.domain.entities.Artist.EmptyArtist
 import ayds.lisboa.songinfo.moredetails.presentation.ArtistInfoResolver
 import ayds.lisboa.songinfo.moredetails.presentation.ArtistInfoResolverImpl
 import io.mockk.every
@@ -22,7 +23,7 @@ class ArtistInfoResolverTest {
         artistInfoResolver = ArtistInfoResolverImpl()
     }
     @Test
-    fun `given artist info and name and isLocallyStored, when getFormattedInfo is called, then returns formatted info in html`() {
+    fun `given artist info and name and artist is LastFMArtist and info isLocallyStored, when getFormattedInfo is called, then returns formatted info in html`() {
         // Arrange
         val artistInfo = LastFMArtist("Test artist", "Test bio","Test url",1,true)
         val artistName = "Test artist"
@@ -36,7 +37,7 @@ class ArtistInfoResolverTest {
     }
 
     @Test
-    fun `given artist info and name and not isLocallyStored, when getFormattedInfo is called, then returns formatted info in html`() {
+    fun `given artist info and name and artist is LastFMArtist and info not isLocallyStored, when getFormattedInfo is called, then returns formatted info in html`() {
         // Arrange
         val artistInfo = LastFMArtist("Test artist", "Test bio","Test url",1,false)
         val artistName = "Test artist"
@@ -46,6 +47,48 @@ class ArtistInfoResolverTest {
 
         // Assert
         val expected =  "<html><div width=400><font face=\"arial\">Test bio</font></div></html>"
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `given artist info and name and artist is emptyArtist, when getFormattedInfo is called, then returns no results`() {
+        // Arrange
+        val artistInfo = EmptyArtist
+        val artistName = "Test empty artist"
+
+        // Act
+        val result = artistInfoResolver.getFormattedInfo(artistInfo, artistName)
+
+        // Assert
+        val expected = "<html><div width=400><font face=\"arial\">No results</font></div></html>"
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `given artist info and name and artist is LastFMArtist and info not isLocallyStored and info is empty, when getFormattedInfo is called, then returns formatted info in html`() {
+        // Arrange
+        val artistInfo = LastFMArtist("Test artist", "","Test url",1,false)
+        val artistName = "Test artist"
+
+        // Act
+        val result = artistInfoResolver.getFormattedInfo(artistInfo, artistName)
+
+        // Assert
+        val expected =  "<html><div width=400><font face=\"arial\">No results</font></div></html>"
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `given artist info and name and artist is LastFMArtist and info isLocallyStored and info is empty, when getFormattedInfo is called, then returns formatted info in html`() {
+        // Arrange
+        val artistInfo = LastFMArtist("Test artist", "","Test url",1,true)
+        val artistName = "Test artist"
+
+        // Act
+        val result = artistInfoResolver.getFormattedInfo(artistInfo, artistName)
+
+        // Assert
+        val expected =  "<html><div width=400><font face=\"arial\">[*]</font></div></html>"
         assertEquals(expected, result)
     }
 
