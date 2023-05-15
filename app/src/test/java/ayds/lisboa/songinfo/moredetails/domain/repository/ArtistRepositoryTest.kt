a39b4ffb13c9b0128afe1e6a9aa6b06498911bf7
PalomaHernandez <palohernandez01@gmail.com>
2023-05-15 16:47:54 -0300

    Creación More Details tests

---
+++ app/src/test/java/ayds/lisboa/songinfo/moredetails/domain/repository/ArtistRepositoryTest.kt
@@ -0,0 +1,4 @@
+package ayds.lisboa.songinfo.moredetails.domain.repository
+
+class ArtistRepositoryTest {
+}
---
+++ app/src/test/java/ayds/lisboa/songinfo/moredetails/presenter/OtherInfoPresenterTest.kt
@@ -0,0 +1,4 @@
+package ayds.lisboa.songinfo.moredetails.presenter
+
+class OtherInfoPresenterTest {
+}
