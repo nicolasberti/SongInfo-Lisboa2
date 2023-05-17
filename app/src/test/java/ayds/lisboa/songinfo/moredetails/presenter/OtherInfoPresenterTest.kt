fd56c43f2cbd9a40245b377c638f3f0236acbe89
ezearce <ezequiel.arce@hotmail.com>
2023-05-16 23:34:50 -0300

    Update OtherInfoPresenterTest.kt
    Co-Authored-By: Facundo Diz <71288527+FacundoDiz@users.noreply.github.com>

--- app/src/test/java/ayds/lisboa/songinfo/moredetails/presenter/OtherInfoPresenterTest.kt
+++ app/src/test/java/ayds/lisboa/songinfo/moredetails/presenter/OtherInfoPresenterTest.kt
@@ -1,4 +1,47 @@
 package ayds.lisboa.songinfo.moredetails.presenter
 
+import ayds.lisboa.songinfo.home.view.HomeUiEvent
+import ayds.lisboa.songinfo.home.view.HomeUiState
+import ayds.lisboa.songinfo.moredetails.domain.repository.ArtistRepository
+import ayds.lisboa.songinfo.moredetails.presentation.*
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Before
+import org.junit.Test
+
 class OtherInfoPresenterTest {
+
+    private val artistInfoRepository: ArtistRepository = mockk(relaxUnitFun = true)
+    private val artistInfoResolver: ArtistInfoResolver = mockk(relaxUnitFun = true)
+
+    private val otherInfoPresenter by lazy {
+        OtherInfoPresenterImpl(artistInfoRepository, artistInfoResolver)
+    }
+
+    @Before
+    fun setUp() {
+        //otherInfoPresenter.uiEventObservable
+    }
+
+    @Test
+    fun `should notify uistate on action search event`(){
+        // Arrange
+        val artistName = "artist"
+        val artistInfo: Artist = mockk()
+        val info = "formatted info"
+        val url = "https://google.com.ar"
+        val otherInfoUiState = OtherInfoUiState(info, url)
+
+        every { artistInfoRepository.getArtist(artistName) } returns artistInfo
+        every { artistInfoResolver.getFormattedInfo(artistInfo, artistName) } returns info
+        every { artistInfoResolver.getUrl(artistInfo) } returns url
+
+        // Act
+        otherInfoPresenter.actionSearch(artistName)
+
+        // Assert
+        verify { otherInfoPresenter.notifyState(otherInfoUiState) }
+    }
+
 }
