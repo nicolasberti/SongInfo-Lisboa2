3c68f234cf293bfd82222fdc57d8ba386c6162c5
Nicolás Berti <71272720+nicolasberti@users.noreply.github.com>
2023-05-29 11:32:28 -0300

    Update Card.kt
    Fix no results

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/domain/entities/Card.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/domain/entities/Card.kt
@@ -9,6 +9,6 @@ data class Card(
     var description: String = "",
     val infoUrl: String = "",
     val source: Source,
-    val sourceLogoUrl: String = "",
+    val sourceLogoUrl: String = "https://cdn-icons-png.flaticon.com/512/6134/6134065.png",
     var isLocallyStored: Boolean = false
 )
